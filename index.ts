dfa5e02d591e29b8cfc25f21f67eccb2cd8cecb2
Vivek Nigam <vivek.nigam@rocketium.com>
2023-02-19 18:41:21 +0530

    feat: adding mongoose connection

--- index.ts
+++ index.ts
@@ -1,12 +1,42 @@
 import cors from "cors";
 import express from "express";
+import { config } from "dotenv";
+import { connect, connection } from "mongoose";
 
+config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
-app.use(cors()); 
+app.use(cors());
 app.use(express.json());
+
+connect(process.env.MONGO_URI || "")
+  .then(() => {
+    console.info("[INFO] Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("[ERROR] Failed to connect to MongoDB", err);
+  });
+
+connection.on("connected", () => {
+  console.info("[INFO] Mongoose connected to MongoDB");
+});
+
+connection.on("error", (err) => {
+  console.error("[ERROR] Mongoose connection error", err);
+});
+
+connection.on("disconnected", () => {
+  console.info("[INFO] Mongoose disconnected from MongoDB");
+});
+
+process.on("SIGINT", () => {
+  connection.close(() => {
+    console.info("[INFO] Mongoose connection closed on app termination");
+    process.exit(0);
+  });
+});
 
 app.listen(port, () => {
   console.info(`[INFO] Server Started on PORT: ${port}`);
-})
+});
